1e27076101e2f9dedd88fc3dff9fc31d48e37adf
Thao Ma <MaThao@gmail.com>
2021-03-09 10:41:10 -0700

    Add test for container with cards

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -13,13 +13,15 @@ describe('App', () => {
       .get('input')
       .should('have.length', 2)
   })
-  
+
   it('should have a form section with a shorten please button', () => {
     cy.get('form')
       .get('button')
       .should('have.text', 'Shorten Please!')
   })
 
-
+  it('should have a container with 3 shorten urls', () => {
+    cy.get('.url').should('have.length', 3)
+  })
 
 })
