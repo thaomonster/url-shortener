655323a0bcc6fbc73d032a3cd27aa2c87e81eaaa
Thao Ma <MaThao@gmail.com>
2021-03-09 09:47:12 -0700

    Fix input handleOnChange so that it is seperate

--- src/components/UrlForm/UrlForm.js
+++ src/components/UrlForm/UrlForm.js
@@ -42,8 +42,8 @@ class UrlForm extends Component {
         <input
           type='text'
           placeholder='URL to Shorten...'
-          name='title'
-          value={this.state.title}
+          name='urlToShorten'
+          value={this.state.urlToShorten}
           onChange={e => this.handleNameChange(e)}
         />
 
