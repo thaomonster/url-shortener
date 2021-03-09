f44f81e74570eadd471211c682025020b6baee09
Thao Ma <MaThao@gmail.com>
2021-03-09 09:30:17 -0700

    Add post api call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,3 +8,14 @@ export const getUrls = () => {
       }
     })
 }
+
+export const postNewUrls= () => {
+  return fetch('http://localhost:3001/api/v1/urls')
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error('Sorry we are having trouble adding you new url')
+      }
+    })
+}
