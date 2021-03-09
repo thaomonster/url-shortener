e42165b82601b0f6c0aa242df6ab1886c7907c92
Thao Ma <MaThao@gmail.com>
2021-03-09 10:39:44 -0700

    Add title test

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,6 +1,14 @@
 describe('App', () => {
   beforeEach(() => {
-    
+    cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {fixture: 'mockData.json'})
     cy.visit('http://localhost:3000/');
   })
+
+  it('should have a title', () => {
+    cy.get('h1').should('have.text', 'URL Shortener')
+  })
+
+ 
+
+
 })
